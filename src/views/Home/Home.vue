83b674ddcc9e331a64eb0f4ca7e8095bf6ba714b
carter <cartergray339@gmail.com>
2022-05-21 23:47:34 +0800

    纯净化项目

---
+++ src/views/Home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="Home">
+     home
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {},
+  data () {
+    return {}
+  },
+  created () {
+  },
+  methods: {},
+  mounted () {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
